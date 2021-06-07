6ffed40feacecefaa85a1ce747cec4708779d0f4
Carlos Ureña <curena@ugr.es>
2021-06-07 14:15:53 +0200

    (almost) completed fig 6

--- src/fig06-frustum2.cpp
+++ src/fig06-frustum2.cpp
@@ -29,7 +29,7 @@ int main( int argc, char *argv[] )
    const float 
       l = -0.3,    r = -l,
       b =  0.7*l,  t = -b ,
-      n =  1.0,    f =  2.0;
+      n =  1.0,    f =  1.5;
    
    const auto fm       = FrustumMesh { l, r, b, t, n, f };
    const auto view_vec = Vec3 { 1, 1, 1 };
@@ -47,7 +47,44 @@ int main( int argc, char *argv[] )
    fm.draw_style_1( view_vec );
 
    // draw line along Z- axis from origin to center of front face on the near plane
-   line( {0,0,0}, {0,0,-n}, "line width=0.15mm,color=blue!50!red", "" );
+   //line( {0,0,0}, {0,0,-n}, "line width=0.15mm,color=blue!50!red", "" );
+
+   // lines marking the width and heigth of frustum front face
+
+   // width:
+   line( {l,b,-n}, {l,b-0.4,-n}, "line width=0.1mm,dashed,color=black!80!blue", "" );
+   line( {r,b,-n}, {r,b-0.4,-n}, "line width=0.1mm,dashed,color=black!80!blue", "" );
+   line( {l,b-0.3,-n}, {r,b-0.3,-n}, "<->,>=latex,line width=0.15mm,color=white!40!blue", " node[pos=0.5,below] {$w$}" );
+  
+   // height
+   line( {l-0.4,b,-n}, {l,b,-n}, "line width=0.1mm,dashed,color=black!80!blue", "" );
+   line( {l-0.4,t,-n}, {l,t,-n}, "line width=0.1mm,dashed,color=black!80!blue", "" );
+   line( {l-0.25,b,-n},{l-0.25,t,-n}, "<->,>=latex,line width=0.15mm,color=white!40!blue", " node[pos=0.5,left] {$h$}" );
+
+
+    // angulo relleno correspondiente a hfovy
+   const unsigned ns = 16 ;
+   const float    hfovy_ang_rad = 2.0*std::atan2(0.5*(t-b),n) , 
+                  hfovy_radius  = 0.5 ;
+
+   cout << "   \\fill[fill=red,opacity=0.5]" << endl 
+        << "         (0,0) -- " << endl ;
+   for( unsigned i = 0 ; i <= ns ; i++ )
+   {  const float ang  = hfovy_ang_rad*(-0.5+float(i)/float(ns)) ;
+      const Vec3  vert = hfovy_radius*Vec3{ 0.0, std::sin(ang), -std::cos(ang)} ;
+      cout << "      " << vert << " -- " << endl ;
+   }
+   cout << "         cycle   ;" << endl;
+
+   //
+   cout << "    \\path " << Vec3{0.0,0.0,-hfovy_radius*1.15} << " node[anchor=center] {$\\beta$};" << endl ; 
+
+   // lines on sides (and in front) of hfovy arc 
+   line( {0.0,0.0,0.0}, {0.0,b,-n}, "line width=0.15mm,color=black", "" );
+   line( {0.0,0.0,0.0}, {0.0,t,-n}, "line width=0.15mm,color=black", "" );
+   line( {0.0,b,-n}, {0.0,t,-n}, "line width=0.15mm,color=black", "" );
+
+  
 
    // draw axes
    axes( "{\\mbox{c}}" );
