dc6580ad78b44402b531f670c2dce7cd9112b448
Carlos Ureña <curena@ugr.es>
2021-06-10 19:59:34 +0200

    func 'define_isometric_proj' in utils.h completed

--- src/fig09-persp2.cpp
+++ src/fig09-persp2.cpp
@@ -33,28 +33,20 @@ int main( int argc, char *argv[] )
       b =  0.7*l, t = -b ,
       n = 1.0,    f = 2.0;
 
-   const auto cubo_ndc = CuboidMesh { -1.0, +1.0, -1.0, +1.0, -1.0, +1.0 };
+   const auto cubo_cc = CuboidMesh { -1.0, +1.0, -1.0, +1.0, -1.0, +1.0 };
    const auto mat      =  Mat4_Scale({1,1,-1}) *
                           Mat4_Frustum( l,r,b,t,n,f )*
                           Mat4_Translation( {-0.15, -0.2, -(n+f)/2.0 })*
                           Mat4_Scale({0.3,0.3,0.3}) ; 
    const auto muros_ca = InstancedMesh { MurosCasita {1.3},  mat } ;
-   
-   const auto view_vec = Vec3 { 1.0, 1.0, 1.0 };
+   const auto view_vec = Vec3 { 1.0, 1.0, 0.5 };
 
-   // cout
-   //    << "\\definecolor{verde}{rgb}{0,0.3,0}" << endl 
-   //    << "\\tikzset{isometric_proj/.style={x={(0.7cm,-0.65cm)},   " << endl
-   //    << "                             y={(0.0cm,0.95cm)},     " << endl
-   //    << "                             z={(-0.7cm,-0.35cm)}}} " << endl ;
-   isometric_proj( view_vec );
+   define_isometric_proj( view_vec );
    cout << "\\begin{tikzpicture}[scale=2,isometric_proj]" << endl ;
 
    muros_ca.draw_style_2( view_vec );
-   cubo_ndc.draw_style_2( view_vec );
+   cubo_cc.draw_style_2( view_vec );
    
-   //mc.draw_normals( 0.5, "->,>=latex,color=blue");
-
    // draw line along Z- axis from origin to center of front face on the near plane
    line( {0,0,0}, {0,0,-n}, "line width=0.15mm,color=blue!50!red", "" );
 
--- src/utils.h
+++ src/utils.h
@@ -516,40 +516,23 @@ void axes_z_neg( const std::string & subscript )
 // ----------------------------------------------------------------------
 
 
-void isometric_proj( const Vec3 & view_vec_nn )
+void define_isometric_proj( const Vec3 & view_vec_nn )
 {
    const float lsq = view_vec_nn.lengthSq()  ;
    assert( lsq >  1e-2 );
-   const Vec3 view_vec   = view_vec_nn/ std::sqrtf( lsq ) ;
-   const Vec3 cam_z_axis = view_vec ;
-   const Vec3 cam_x_axis = (Vec3(0.0,1.0,0.0).cross( view_vec )).normalized() ;
-   const Vec3 cam_y_axis = (cam_z_axis.cross( cam_x_axis )).normalized();
-
-   cerr << " z axis == " << cam_z_axis << endl ;
+    
+   const Vec3   // camera coordinates axes, in world coordinates
+      z_cam = view_vec_nn/ std::sqrtf( lsq ) ,
+      x_cam = Vec3(0.0,1.0,0.0).cross( z_cam ).normalized() ,
+      y_cam = z_cam.cross( x_cam ).normalized();
    
-   cerr << "x·x == " << cam_x_axis.dot( cam_x_axis ) << endl ;
-   cerr << "y·y == " << cam_y_axis.dot( cam_y_axis ) << endl ;
-   cerr << "z·z == " << cam_z_axis.dot( cam_z_axis ) << endl ;
-
-   cerr << "x·y == " << cam_x_axis.dot( cam_y_axis ) << endl ;
-   cerr << "y·z == " << cam_y_axis.dot( cam_z_axis ) << endl ;
-   cerr << "z·x == " << cam_z_axis.dot( cam_x_axis ) << endl ;
-
-   const auto xa = Vec2 { cam_x_axis(0), cam_y_axis(0) } ;
-   const auto ya = Vec2 { cam_x_axis(1), cam_y_axis(1) } ;
-   const auto za = Vec2 { cam_x_axis(2), cam_y_axis(2) } ;
-
-
-   cerr 
-      << "xa == " << xa << endl 
-      << "ya == " << ya << endl 
-      << "za == " << za << endl ;
-        
-
-   cout << "\\tikzset{isometric_proj/.style={x={" << xa << "}, y={" << ya << "}, z={" << za << "}}} " << endl ;
+   cout 
+      << "\\tikzset{isometric_proj/.style={" << endl 
+      << "            x={(" << x_cam(0) << "cm," << y_cam(0) << "cm)}," << endl 
+      << "            y={(" << x_cam(1) << "cm," << y_cam(1) << "cm)}," << endl
+      << "            z={(" << x_cam(2) << "cm," << y_cam(2) << "cm)}"  << endl 
+      << "}}" << endl ; 
 }
-
-
 
 } // end namespace utils 
 
